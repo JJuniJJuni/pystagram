5fe901efe84a692e3863425edb1f646482ca9a33
JJuniJJuni <gud305@gmail.com>
2018-03-25 11:16:09 +0900

    make edit.html

--- photos/models.py
+++ photos/models.py
@@ -4,10 +4,11 @@ from django.db import models
 
 
 class Photo(models.Model):
-    image=models.ImageField(upload_to="%Y/%m/%d/orig")
-    filtered_image=models.ImageField(upload_to="%Y/%m/%d/filtered")
-    content=models.TextField(max_length=500,blank=True,null=True)
-    created_at=models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="%Y/%m/%d/orig")
+    filtered_image = models.ImageField(upload_to="%Y/%m/%d/filtered")
+    content = models.TextField(max_length=500,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def delete(self, *args, **kwargs):
         self.image.delete()
