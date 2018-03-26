98624025ee8346b90d07e8b83e21a244ed024985
gud305 <hyoungjun.park@rgpkorea.co.kr>
2018-03-26 20:49:25 +0900

    나머지 커밋

---
+++ profiles/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from django.shortcuts import render
+
+
+# Create your views here.
+def profile(request, username):
+    # url에서 username을 변수로 전해줬으므로, 매개변수로 받아야함
+    User = get_user_model()
+    # settings.py 모듈의 AUTH_USER_MODEL 설정 항목 기준으로
+    # django 프로젝트가 사용하는 이용자 모델을 가져온다.
+    user = get_object_or_404(User, username=username)
+    # django에서 제공하는 User 모델을 사용하므로
+    # 이 모델의 username 모델 필드를 검색하는데 사용하여 이용자 찾는다
+    # 첫번 째 인자(지정한 모델)과 두번째 인자(검색 조건)으로 데이터 가져옴
+    ctx = {
+        'user': user,
+    }
+    return render(request, 'profile.html', ctx)
