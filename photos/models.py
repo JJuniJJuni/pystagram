ceabd5961dbe83137c24e75cbf62ff4b1137c26d
gud305 <hyoungjun.park@rgpkorea.co.kr>
2018-03-26 16:19:40 +0900

    new migrations

--- photos/models.py
+++ photos/models.py
@@ -1,17 +1,19 @@
 from django.db import models
-from django.urls import reverse
+from django.core.urlresolvers import reverse_lazy
+from django.conf import settings
 
 # Create your models here.
 
 
 class Photo(models.Model):  # 각 파일의 형식 지정
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     image = models.ImageField(upload_to="%Y/%m/%d/orig")
     filtered_image = models.ImageField(upload_to="%Y/%m/%d/filtered")
     content = models.TextField(max_length=500,blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):  # 클라이언트의 사진 pk를 넣고 반환
-        url = reverse("detail", kwargs={"pk": self.pk})
+        url = reverse_lazy("detail", kwargs={"pk": self.pk})
         return url
 
     def delete(self, *args, **kwargs):
