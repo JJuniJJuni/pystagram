e34cbd66a5327017cb4680fd77155db40284879f
gud305 <hyoungjun.park@rgpkorea.co.kr>
2018-03-27 20:17:50 +0900

    complete hannal

--- profiles/views.py
+++ profiles/views.py
@@ -12,8 +12,17 @@ def profile(request, username):
     user = get_object_or_404(User, username=username)
     # django에서 제공하는 User 모델을 사용하므로
     # 이 모델의 username 모델 필드를 검색하는데 사용하여 이용자 찾는다
-    # 첫번 째 인자(지정한 모델)과 두번째 인자(검색 조건)으로 데이터 가져옴
+    # 첫번 째 인자(지정한 모델)과 두번째 인자(검색 조건)으로 데이터 가져
+
+    # 연결 당하는 쪽은 해당 필드를 갖고 있지 않음
+    # 그래서 연결 당하는 쪽인 User 모델에 대한 접근 경로를
+    # QuerySet 객체 속성으로 만들어 줌.
+    # QuerySet은 사용자의 요청을 받아 질의를 생성.
+    # 이 객채로 요청을 주고 받을 수 있음!!
+    photos = user.photo_set.order_by('-created_at', '-pk')
     ctx = {
+        'photos': photos,
         'user': user,
     }
+
     return render(request, 'profile.html', ctx)
